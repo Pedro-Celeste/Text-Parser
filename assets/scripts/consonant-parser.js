740a1646e07ef35b12452b6d442ca54e0fe35fa0
pedroceleste <work.pedroaugusto@gmail.com>
2023-03-07 19:45:03 -0300

    Bug Fix: Letter K now works!

--- assets/scripts/consonant-parser.js
+++ assets/scripts/consonant-parser.js
@@ -19,9 +19,9 @@ inputField.addEventListener(
 function howManyConsonants(string) {
   const consonants = [
     "b", "c", "d", "f", "g", "h",
-    "j", "l", "m", "n", "p", "q",
-    "r", "s", "t", "v", "w", "x",
-    "y", "z", "ç", "ñ",
+    "j", "k", "l", "m", "n", "p",
+    "q","r", "s", "t", "v", "w",
+    "x", "y", "z", "ç", "ñ",
   ];
   let counter = 0;
   
